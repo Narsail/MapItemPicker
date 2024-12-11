d1487084eefc9db553b3aad13396178a35d98150
David Moeller <david.moller@sensorfact.nl>
2024-12-11 10:40:53 +0100

    Add additional initializer for MapItemDisplayView

--- Sources/MapItemPicker/UI/MapItemDisplayView/MapItemDisplayView.swift
+++ Sources/MapItemPicker/UI/MapItemDisplayView/MapItemDisplayView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 public struct MapItemDisplayView: View {
     let mapItem: MapItem
     let shouldScroll: Bool
+    let primaryAction: MapItemPickerAction?
+    let actions: [MapItemPickerAction]
     
     @State private var coordinator: MapItemController?
     
@@ -10,14 +12,26 @@ public struct MapItemDisplayView: View {
         self.mapItem = mapItem
         self.shouldScroll = shouldScroll
     }
+
+    public init(
+        mapItem: MapItem, 
+        primaryAction: MapItemPickerAction? = nil,
+        actions: [MapItemPickerAction] = [],
+        shouldScroll: Bool = true
+    ) {
+        self.mapItem = mapItem
+        self.shouldScroll = shouldScroll
+        self.primaryAction = primaryAction
+        self.actions = actions
+    }
     
     public var body: some View {
         if let coordinator {
             MapItemDisplaySheet(
                 coordinator: nil,
                 itemCoordinator: coordinator,
-                primaryAction: nil,
-                actions: [],
+                primaryAction: primaryAction,
+                actions: actions,
                 dismissHandler: nil,
                 shouldScroll: shouldScroll,
                 shouldAddPadding: false
